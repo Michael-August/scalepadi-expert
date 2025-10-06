054b08f355c1fa1c6ef9a432d8a43cc5943e4f8b
Chibuike Onwugharam <michaelfrancis135@gmail.com>
2025-10-06 07:22:06 +0100

    Protected on nosidebar layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,15 +2,15 @@ import AuthNav from "@/components/auth-nav";
 import Footer from "@/components/footer";
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <main className="flex flex-col lg:w-[1440px] lg:max-w-[1440px] lg:mx-auto w-screen">
-            <AuthNav />
-            {children}
-            <Footer />
-        </main>
-    );
+	return (
+		<main className="flex flex-col lg:w-[1440px] lg:max-w-[1440px] lg:mx-auto w-screen">
+			<AuthNav />
+			{children}
+			<Footer />
+		</main>
+	);
 }
--- src/app/(authenticated)/(noSideBar)/layout.tsx
+++ src/app/(authenticated)/(noSideBar)/layout.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import DashboardNav from "@/components/dashboard-navbar";
 import Footer from "@/components/footer";
+import Protected from "@/components/wrappers/Protected";
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <main className="flex flex-col lg:w-[1440px] lg:max-w-[1440px] lg:mx-auto w-screen">
-            <DashboardNav />
-            {children}
-            <Footer />
-        </main>
-    );
+	return (
+		<Protected>
+			<main className="flex flex-col lg:w-[1440px] lg:max-w-[1440px] lg:mx-auto w-screen">
+				<DashboardNav />
+				{children}
+				<Footer />
+			</main>
+		</Protected>
+	);
 }
